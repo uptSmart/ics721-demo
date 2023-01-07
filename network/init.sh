cd33040c0c1e2bff39c1d26a070abe9447d04ab0
uptSmart <uptSmart@163.com>
2023-01-07 09:58:36 +0000

    init the demo for iris and uptick chain

--- network/init.sh
+++ network/init.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 
-BINARY=nftd
 CHAIN_DIR=./data
+
+BINARY1=iris
+CHAIN1_BOND_DENOM=uiris
+
+BINARY2=uptickd
+CHAIN2_BOND_DENOM=auptick
+
 CHAINID_1=test-1
-CHAINID_2=test-2
+CHAINID_2=uptick_7000-1
+
 VAL_MNEMONIC_1="clock post desk civil pottery foster expand merit dash seminar song memory figure uniform spice circle try happy obvious trash crime hybrid hood cushion"
 VAL_MNEMONIC_2="angry twist harsh drastic left brass behave host shove marriage fall update business leg direct reward object ugly security warm tuna model broccoli choice"
+
 DEMO_MNEMONIC_1="banner spread envelope side kite person disagree path silver will brother under couch edit food venture squirrel civil budget number acquire point work mass"
 DEMO_MNEMONIC_2="veteran try aware erosion drink dance decade comic dawn museum release episode original list ability owner size tuition surface ceiling depth seminar capable only"
+
 RLY_MNEMONIC_1="alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart"
 RLY_MNEMONIC_2="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
+
 P2PPORT_1=16656
 P2PPORT_2=26656
+
 RPCPORT_1=16657
 RPCPORT_2=26657
+
 RPCAPPPORT_1=16658
 RPCAPPPORT_2=26658
 
 RESTPORT_1=1316
 RESTPORT_2=1317
+
 ROSETTA_1=8080
 ROSETTA_2=8081
 
-# Stop if it is already running 
-if pgrep -x "$BINARY" >/dev/null; then
-    echo "Terminating $BINARY..."
-    killall $BINARY
+#Stop iris if it is already running 
+if pgrep -x "$BINARY1" >/dev/null; then
+    echo "Terminating $BINARY1..."
+    killall $BINARY1
+fi
+
+#Stop uptickd if it is already running 
+if pgrep -x "$BINARY2" >/dev/null; then
+    echo "Terminating $BINARY2..."
+    killall $BINARY2
 fi
 
 echo "Removing previous data..."
@@ -44,30 +63,34 @@ if ! mkdir -p $CHAIN_DIR/$CHAINID_2 2>/dev/null; then
 fi
 
 echo "Initializing $CHAINID_1..."
+$BINARY1 init test --home $CHAIN_DIR/$CHAINID_1 --chain-id=$CHAINID_1 --staking-bond-denom=$CHAIN1_BOND_DENOM
+
 echo "Initializing $CHAINID_2..."
-$BINARY init test --home $CHAIN_DIR/$CHAINID_1 --chain-id=$CHAINID_1
-$BINARY init test --home $CHAIN_DIR/$CHAINID_2 --chain-id=$CHAINID_2
+$BINARY2 init test --home $CHAIN_DIR/$CHAINID_2 --chain-id=$CHAINID_2 --staking-bond-denom=$CHAIN2_BOND_DENOM
 
 echo "Adding genesis accounts..."
-echo $VAL_MNEMONIC_1 | $BINARY keys add val1 --home $CHAIN_DIR/$CHAINID_1 --recover --keyring-backend=test
-echo $VAL_MNEMONIC_2 | $BINARY keys add val2 --home $CHAIN_DIR/$CHAINID_2 --recover --keyring-backend=test
-echo $DEMO_MNEMONIC_1 | $BINARY keys add demowallet1 --home $CHAIN_DIR/$CHAINID_1 --recover --keyring-backend=test
-echo $DEMO_MNEMONIC_2 | $BINARY keys add demowallet2 --home $CHAIN_DIR/$CHAINID_2 --recover --keyring-backend=test
-echo $RLY_MNEMONIC_1 | $BINARY keys add rly1 --home $CHAIN_DIR/$CHAINID_1 --recover --keyring-backend=test 
-echo $RLY_MNEMONIC_2 | $BINARY keys add rly2 --home $CHAIN_DIR/$CHAINID_2 --recover --keyring-backend=test 
+echo $VAL_MNEMONIC_1 | $BINARY1 keys add val1 --home $CHAIN_DIR/$CHAINID_1 --recover --keyring-backend=test
+echo $DEMO_MNEMONIC_1 | $BINARY1 keys add demowallet1 --home $CHAIN_DIR/$CHAINID_1 --recover --keyring-backend=test
+echo $RLY_MNEMONIC_1 | $BINARY1 keys add rly1 --home $CHAIN_DIR/$CHAINID_1 --recover --keyring-backend=test 
 
-$BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_1 keys show val1 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_1
-$BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_2 keys show val2 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_2
-$BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_1 keys show demowallet1 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_1
-$BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_2 keys show demowallet2 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_2
-$BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_1 keys show rly1 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_1
-$BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_2 keys show rly2 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_2
+echo $VAL_MNEMONIC_2 | $BINARY2 keys add val2 --home $CHAIN_DIR/$CHAINID_2 --recover --keyring-backend=test
+echo $DEMO_MNEMONIC_2 | $BINARY2 keys add demowallet2 --home $CHAIN_DIR/$CHAINID_2 --recover --keyring-backend=test
+echo $RLY_MNEMONIC_2 | $BINARY2 keys add rly2 --home $CHAIN_DIR/$CHAINID_2 --recover --keyring-backend=test 
+
+$BINARY1 add-genesis-account $($BINARY1 --home $CHAIN_DIR/$CHAINID_1 keys show val1 --keyring-backend test -a) 100000000000$CHAIN1_BOND_DENOM  --home $CHAIN_DIR/$CHAINID_1
+$BINARY1 add-genesis-account $($BINARY1 --home $CHAIN_DIR/$CHAINID_1 keys show demowallet1 --keyring-backend test -a) 100000000000$CHAIN1_BOND_DENOM  --home $CHAIN_DIR/$CHAINID_1
+$BINARY1 add-genesis-account $($BINARY1 --home $CHAIN_DIR/$CHAINID_1 keys show rly1 --keyring-backend test -a) 100000000000$CHAIN1_BOND_DENOM  --home $CHAIN_DIR/$CHAINID_1
+
+$BINARY2 add-genesis-account $($BINARY2 --home $CHAIN_DIR/$CHAINID_2 keys show val2 --keyring-backend test -a) 100000000000000000000000000$CHAIN2_BOND_DENOM  --home $CHAIN_DIR/$CHAINID_2
+$BINARY2 add-genesis-account $($BINARY2 --home $CHAIN_DIR/$CHAINID_2 keys show demowallet2 --keyring-backend test -a) 100000000000000000000000000$CHAIN2_BOND_DENOM  --home $CHAIN_DIR/$CHAINID_2
+$BINARY2 add-genesis-account $($BINARY2 --home $CHAIN_DIR/$CHAINID_2 keys show rly2 --keyring-backend test -a) 100000000000000000000000000$CHAIN2_BOND_DENOM  --home $CHAIN_DIR/$CHAINID_2
 
 echo "Creating and collecting gentx..."
-$BINARY gentx val1 7000000000stake --home $CHAIN_DIR/$CHAINID_1 --chain-id $CHAINID_1 --keyring-backend test
-$BINARY gentx val2 7000000000stake --home $CHAIN_DIR/$CHAINID_2 --chain-id $CHAINID_2 --keyring-backend test
-$BINARY collect-gentxs --home $CHAIN_DIR/$CHAINID_1
-$BINARY collect-gentxs --home $CHAIN_DIR/$CHAINID_2
+$BINARY1 gentx val1 7000000000$CHAIN_BOND_DENOM --home $CHAIN_DIR/$CHAINID_1 --chain-id $CHAINID_1 --keyring-backend test
+$BINARY1 collect-gentxs --home $CHAIN_DIR/$CHAINID_1
+
+$BINARY2 gentx val2 70000000000000000000000000$CHAIN_BOND_DENOM --home $CHAIN_DIR/$CHAINID_2 --chain-id $CHAINID_2 --keyring-backend test
+$BINARY2 collect-gentxs --home $CHAIN_DIR/$CHAINID_2
 
 echo "Changing defaults and ports in app.toml and config.toml files..."
 sed -i -e 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:'"$P2PPORT_1"'"#g' $CHAIN_DIR/$CHAINID_1/config/config.toml
--- network/start.sh
+++ network/start.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-BINARY=nftd
+BINARY1=iris
+BINARY2=uptickd
+
 CHAIN_DIR=./data
+
 CHAINID_1=test-1
-CHAINID_2=test-2
 GRPCPORT_1=8090
-GRPCPORT_2=9090
 GRPCWEB_1=8091
+
+CHAINID_2=uptick_7000-1
+GRPCPORT_2=9090
 GRPCWEB_2=9091
 
+TRACE=""
+LOGLEVEL="info"
 
 echo "Starting $CHAINID_1 in $CHAIN_DIR..."
 echo "Creating log file at $CHAIN_DIR/$CHAINID_1.log"
-$BINARY start --log_format json --home $CHAIN_DIR/$CHAINID_1 --pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT_1" --grpc-web.address="0.0.0.0:$GRPCWEB_1" > $CHAIN_DIR/$CHAINID_1.log 2>&1 &
+$BINARY1 start --log_format json --home $CHAIN_DIR/$CHAINID_1 \
+--pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT_1" \
+--grpc-web.address="0.0.0.0:$GRPCWEB_1" > $CHAIN_DIR/$CHAINID_1.log 2>&1 &
 
 echo "Starting $CHAINID_2 in $CHAIN_DIR..."
 echo "Creating log file at $CHAIN_DIR/$CHAINID_2.log"
-$BINARY start --log_format json --home $CHAIN_DIR/$CHAINID_2 --pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT_2" --grpc-web.address="0.0.0.0:$GRPCWEB_2" > $CHAIN_DIR/$CHAINID_2.log 2>&1 &
+# $BINARY 2start --log_format json --home $CHAIN_DIR/$CHAINID_2 --pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT_2" --grpc-web.address="0.0.0.0:$GRPCWEB_2" > $CHAIN_DIR/$CHAINID_2.log 2>&1 &
+
+$BINARY2 start $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0auptick \
+--json-rpc.api eth,txpool,personal,net,debug,web3 \
+--log_format json --home $CHAIN_DIR/$CHAINID_2 \
+--pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT_2" \
+--grpc-web.address="0.0.0.0:$GRPCWEB_2" > $CHAIN_DIR/$CHAINID_2.log 2>&1 &
+
+sleep 8
+
+echo "TODO ..."
+uptickd tx bank send uptick103rx84uqa7n4mtmz8f88n4g9m7973rxutrtn7d uptick1p4szayprev5yml5f2l6uq39gyuzamq3j9kqr2a \
+1000000000auptick --chain-id uptick_7000-1 --home ./data/uptick_7000-1 \
+--node tcp://localhost:26657 --keyring-backend test -y
+
+
+
